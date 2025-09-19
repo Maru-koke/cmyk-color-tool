d0e0bbf3bb27b1b7e39f89b36dbb7693613fc699
Maru-Koke <kouketsu@maruwanet.co.jp>
2025-09-19 13:45:40 +0900

    move pages into src

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'; // ←ここを相対パスに変更
+import '../../styles/globals.css'; // ←ここを相対パスに変更
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
